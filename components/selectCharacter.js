c82142bfb14144f76bcbd9027a690cd40dd3e2b5
Ahmed Alahmady <aagameac@gmail.com>
2017-08-11 05:27:04 -0500

    add ramda, ramda babel plugin, isomorphic-fetch

--- components/selectCharacter.js
+++ components/selectCharacter.js
@@ -1,9 +1,19 @@
+import { map } from 'ramda'
 import styled from 'styled-components'
 
-export default ({ characterImage, characterSFX, characterName, className }) =>
-  <div className={className}>
-    <img src={characterImage} />
-    <audio src={characterSFX} />
+export default ({
+  actions: { openDriverPanel, selectDriver },
+  data: { allCharacters = [] },
+  className
+}) =>
+  <div
+    className={className}
+    onClick={() => {
+      openDriverPanel({ selectedDriver: id }), openDriverPanel()
+    }}
+  >
+    <img src={image.url} />
+    <audio src={characterSFX.url} />
     <p>
       {characterName}
     </p>
