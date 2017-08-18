31c9370e6d467f9fd75e11442b7222b93583a0fc
Ahmed Alahmady <aagameac@gmail.com>
2017-08-18 00:16:07 -0500

    no need to pass server state (...props object spread) into a "main" element, since it doesn't understand what it is anyways. since non-changing props were used (for the sake of this demo), remove imageAlt, imageSrc, subHeading, and title values from the DriverPanelContainer. Remove SelectDriverContainer from this export since it's now being rendered in the DriverPanelContainer

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import DriverPanelContainer from '../containers/DriverPanelContainer'
-import SelectDriverContainer from '../containers/SelectDriverContainer'
 import WithData from '../lib/withData'
 import globalStyle from '../globalStyle'
 
 export default WithData(props =>
-  <main {...props}>
+  <main>
     <div className="animatedBg" />
     <div className="black-gradient" />
-    <DriverPanelContainer
-      imageAlt="Broken Hearts at Match"
-      imageSrc="./static/brokenHeart.svg"
-      subHeading="There is no love found on these streets."
-      title="Mario Kart. Match Style."
-    >
-      <SelectDriverContainer />
-    </DriverPanelContainer>
+    <DriverPanelContainer />
   </main>
 )
